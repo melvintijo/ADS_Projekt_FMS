a9de7b2f2b96908cf18dd07121ac610771ef3192
melvintijo <73169012+melvintijo@users.noreply.github.com>
2021-05-24 11:13:14 +0200

    Add files via upload

---
+++ API/Pexel/Pexels.py
@@ -0,0 +1,44 @@
+import os
+from urllib.parse import urlparse
+import requests
+# Import API class from pexels_api package
+from pexels_api import API
+from requests.structures import CaseInsensitiveDict
+# Type your Pexels API
+PEXELS_API_KEY = '563492ad6f9170000100000128f0f5bd9d114207967cd6461c6e6f14'
+# Create API object
+api = API(PEXELS_API_KEY)
+api.search('cat', page=90, results_per_page=100)
+# Get photo entries
+photos = api.get_entries()
+headers = CaseInsensitiveDict()
+headers["Authorization"] = "xxx"
+# Loop the five photos
+for photo in photos:
+  # Print photographer
+  print('Photographer: ', photo.photographer)
+  # Print url
+  print('Photo url: ', photo.url)
+  # Print original size url
+  print('Photo original size: ', photo.large)
+  a = urlparse(photo.large)
+  filename = os.path.basename(a.path)
+  r = requests.get(photo.large, headers=headers)
+  open('Cats/'+filename, 'wb').write(r.content)
+
+api.search('dog', page=90, results_per_page=100)
+# Get photo entries
+photos = api.get_entries()
+headers = CaseInsensitiveDict()
+headers["Authorization"] = "xxx"
+for photo in photos:
+  # Print photographer
+  print('Photographer: ', photo.photographer)
+  # Print url
+  print('Photo url: ', photo.url)
+  # Print original size url
+  print('Photo original size: ', photo.large)
+  a = urlparse(photo.large)
+  filename = os.path.basename(a.path)
+  r = requests.get(photo.large, headers=headers)
+  open('dogs/'+filename, 'wb').write(r.content)
